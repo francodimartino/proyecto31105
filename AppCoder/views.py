1818afb1452b26b86a47fa30b446327d8b527b91
FrancoDiMartino <francogdimartino@gmail.com>
2022-08-22 21:00:50 -0300

    fin clase 18

---
+++ AppCoder/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from .models import Curso
+from django.http import HttpResponse
+# Create your views here.
+
+def curso(request):
+    curso=Curso(nombre="AWS", comision=3)
+    curso2=Curso(nombre="Lengua", comision=1)
+    curso3=Curso(nombre="Matematica", comision=2)
+    curso.save()
+    curso2.save()
+    curso3.save()
+    texto=f"cursos creados"
+    return HttpResponse(texto)
+
+
